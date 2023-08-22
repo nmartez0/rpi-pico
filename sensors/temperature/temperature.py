1fce35bd70971a7b02ec1f0dfb2241a6a9651799
Jose Ignacio Martin <jimartinfdez@outlook.com>
2023-08-22 18:47:05 +0200

    Added screen as output

--- sensors/temperature/temperature.py
+++ sensors/temperature/temperature.py
@@ -1,13 +1,23 @@
 import machine
 import utime
-from dht import DHT11
+import dht
+import ssd1306
 
-pin = machine.Pin(28, machine.Pin.OUT, machine.Pin.PULL_DOWN)
-sensor = DHT11(pin)
+# DHT11 Sensor
+sensor_pin = machine.Pin(0)
+sensor = dht.DHT11(sensor_pin)
+result = sensor.measure()
+
+# Display 128x32 
+WIDTH = 128
+HEIGH = 32
+i2c = machine.I2C(0,sda=machine.Pin(4), scl=machine.Pin(5))
+display = ssd1306.SSD1306_I2C(WIDTH,HEIGH,i2c)
 
 while True:
-    t  = (sensor.temperature)
-    h = (sensor.humidity)
-    print("Temperature: {}".format(sensor.temperature()))
-    print("Humidity: {}".format(sensor.humidity()))
+    temperature  = (sensor.temperature())
+    humidity = (sensor.humidity())
+    display.text("Temp: {:.01f}C".format(temperature), 5,8)
+    display.text("Hum:  {:.01f}%".format(humidity), 5, 16)
+    display.show()
     utime.sleep(2)
